63255b288e70ad19d81c9221089143e1d73e0e81
ealpha072 <ealpha072@gmail.com>
2021-01-12 13:55:26 +0300

    Login form

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php require "header.php";?>
+<h3>Login</h3>
+<form action="" method="post">
+  <div class="form-group">
+      <label for="email">Email address</label>
+      <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+    </div>
+  <div class="form-group">
+      <label for="password">Password</label>
+      <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+
+  <div class="register">
+    <p>Not yet a member? <a href="registration.php">Register</a></p>
+  </div>
+</form>
+<?php require "footer.php";?>

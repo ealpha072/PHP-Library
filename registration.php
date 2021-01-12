88bcf8a4d89addd7485a528b52104e2258b0f18c
ealpha072 <ealpha072@gmail.com>
2021-01-12 14:17:23 +0300

    Name attributes added

--- registration.php
+++ registration.php
@@ -1,9 +1,11 @@
-<?php require "header.php";?>
-
-<form>
+<?php require "header.php";
+      require "config.php";
+?>
+<h4>Register to be a member</h4>
+<form action="registration.php" method="post">
   <div class="form-group">
     <label for="email">Email address</label>
-    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="">
+    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
@@ -18,6 +20,9 @@
     <label for="password">Confirm Password</label>
     <input type="password" class="form-control" id="" placeholder="Confirm Password" name="password_2">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary" name="register">Submit</button>
 </form>
+<div class="login">
+  <p>Already a member? <a href="login.php">Login</a></p>
+</div>
 <?php require "footer.php";?>

2da1cdd0e883ca7ac45a43ecac7a932123390bcc
ealpha072 <ealpha072@gmail.com>
2021-01-12 13:14:44 +0300

    Footer and header files

---
+++ header.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv = "x-ua-compatible" content="ie=edge"/>
+  <title>Simple Database App</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+	crossorigin="anonymous">
+</head>
+<body>
+ <div class="container">
+  <div class="head">
+    <h1>Simple Database App</h1>
+  </div>

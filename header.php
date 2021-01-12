9b00ccfd23e3b5378ef116d6bfe36da616e018db
ealpha072 <ealpha072@gmail.com>
2021-01-12 13:16:09 +0300

    Changed title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv = "x-ua-compatible" content="ie=edge"/>
-  <title>Simple Database App</title>
+  <title>Alpha Library</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
 	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
 	crossorigin="anonymous">
@@ -11,5 +11,5 @@
 <body>
  <div class="container">
   <div class="head">
-    <h1>Simple Database App</h1>
+    <h1>Alpha's Library</h1>
   </div>

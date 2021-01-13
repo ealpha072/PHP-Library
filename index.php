f3ae9d8243ae852b2a92965b017375849334754f
ealpha072 <ealpha072@gmail.com>
2021-01-13 14:50:15 +0300

    Required fields

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
   session_start();
+
+  require "header.php";
   if(!isset($_SESSION['loggedin'])){
     $_SESSION['msg']= "You must be logged in";
     header("location:login.php");
   }
 
+
+
 ?>
 
-<h1>Welcome home <?php echo $_SESSION['username'];?></h1>
+<div>
+  <h1>Welcome home <?php echo $_SESSION['username'];?></h1>
+</div>
+<p><a href="">Logout</a></p>
+
+<?php require "footer.php";?>

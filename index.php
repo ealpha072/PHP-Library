fe269b95e17c92e841484bd15c9e3c32c2f37544
ealpha072 <ealpha072@gmail.com>
2021-01-13 17:25:57 +0300

    Bug fixes

--- index.php
+++ index.php
@@ -11,6 +11,6 @@
 <div>
   <h1>Welcome home <?php echo ucfirst($_SESSION['username']);?></h1>
 </div>
-<p><a href="logout.php">Logout</a></p>
 
-<?php require "footer.php";?>
+
+<?php require "infooter.php";?>

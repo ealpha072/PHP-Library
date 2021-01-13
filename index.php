262b7298651f6178bf38038f0c1b98c09280f1a6
ealpha072 <ealpha072@gmail.com>
2021-01-13 14:53:03 +0300

    ucfirst for username conversion

--- index.php
+++ index.php
@@ -6,13 +6,10 @@
     $_SESSION['msg']= "You must be logged in";
     header("location:login.php");
   }
-
-
-
 ?>
 
 <div>
-  <h1>Welcome home <?php echo $_SESSION['username'];?></h1>
+  <h1>Welcome home <?php echo ucfirst($_SESSION['username']);?></h1>
 </div>
 <p><a href="">Logout</a></p>
 

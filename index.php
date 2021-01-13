33aa83b14f8ff1f038507dbf89f40fbdc1484619
ealpha072 <ealpha072@gmail.com>
2021-01-13 14:29:38 +0300

    Logic for logged in user

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
   session_start();
-  if(!isset($_SESSION['username'])){
+  if(!isset($_SESSION['loggedin'])){
     $_SESSION['msg']= "You must be logged in";
     header("location:login.php");
   }
 
 ?>
 
-<h1>Welcome home <?php $_SESSION['username'];?></h1>
+<h1>Welcome home <?php echo $_SESSION['username'];?></h1>

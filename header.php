0341e412357825e096c21574c18ddb86cff69b8a
ealpha072 <ealpha072@gmail.com>
2021-01-17 17:46:22 +0300

    Styles for sidebar

--- header.php
+++ header.php
@@ -7,6 +7,20 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
 	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
 	crossorigin="anonymous">
+  <style type="text/css"> 
+        html, 
+        body { 
+            height: 100%; 
+        } 
+  
+        #green { 
+            height: 100%; 
+            background: green; 
+            text-align: center; 
+            color: black; 
+            padding: 15px; 
+        } 
+    </style> 
 </head>
 <body>
  <div class="container-fluid">

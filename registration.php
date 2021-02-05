58e8fcdb1218ebb22d5c856108ba56ce3f9ccc3b
ealpha072 <ealpha072@gmail.com>
2021-02-05 17:57:52 +0300

    Styling of form

--- registration.php
+++ registration.php
@@ -3,28 +3,29 @@
 ?>
 <h4>Register to be a member</h4>
 
-<!--<form action="registration.php" method="post">
-  <div class="form-group">
-    <label for="email">Email address</label>
-    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+<main class="main">
+  <form action="registration.php" method="post">
+    <div class="form-group">
+      <label for="email">Email address</label>
+      <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+      <label for="username">Username</label>
+      <input type="text" class="form-control" id="username" placeholder="Username" name="username">
+    </div>
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input type="password" class="form-control" id="password" placeholder="Password" name="password_1">
+    </div>
+    <div class="form-group">
+      <label for="password">Confirm Password</label>
+      <input type="password" class="form-control" id="" placeholder="Confirm Password" name="password_2">
+    </div>
+    <button type="submit" class="btn btn-primary" name="register">Submit</button>
+  </form>
+  <div class="login">
+    <p>Already a member? <a href="login.php">Login</a></p>
   </div>
-  <div class="form-group">
-    <label for="username">Username</label>
-    <input type="text" class="form-control" id="username" placeholder="Username" name="username">
-  </div>
-  <div class="form-group">
-    <label for="password">Password</label>
-    <input type="password" class="form-control" id="password" placeholder="Password" name="password_1">
-  </div>
-  <div class="form-group">
-    <label for="password">Confirm Password</label>
-    <input type="password" class="form-control" id="" placeholder="Confirm Password" name="password_2">
-  </div>
-  <button type="submit" class="btn btn-primary" name="register">Submit</button>
-</form>
-<div class="login">
-  <p>Already a member? <a href="login.php">Login</a></p>
-</div>-->
-
+</main
 <?php require "footer.php";?>

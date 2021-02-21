7595249740fa804d74a22b81d7cb19c57166ba05
ealpha072 <ealpha072@gmail.com>
2021-02-21 22:33:46 +0300

    Reset password page

---
+++ reset.php
@@ -0,0 +1,15 @@
+<?php
+  //session_start();
+  require "header.php";
+  require "config.php";
+  if(!isset($_SESSION['loggedin'])){
+    $_SESSION['msg']= "You must be logged in";
+    header("location:login.php");
+  }
+?>
+
+<div>
+    <form action="">
+        
+    </form>
+</div>

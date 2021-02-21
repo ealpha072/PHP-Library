cdb519e6398147cb205bbf3862a4c2d56ca12bd8
ealpha072 <ealpha072@gmail.com>
2021-02-21 19:01:58 +0300

    Changed styles file

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
   crossorigin="anonymous">
   <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
  <div class="container-fluid">

cc4aa762c479d8650a984b2788316a5567305139
ealpha072 <ealpha072@gmail.com>
2021-02-21 23:04:58 +0300

    Modals added

--- profile.php
+++ profile.php
@@ -7,16 +7,34 @@
     header("location:login.php");
   }
 ?>
-<div class="user-profile">
-    <div class="user-image">
-        <img src="images/avatar.png" alt="">
-        <button class="btn btn-primary">Change Profile Picture</button>
+<div class="user-profile">   
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Change Profile Picture</button>
+
+    <!--modal-->
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">New Profile</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+           <form action="">
+                <input type="file">
+           </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" name="profile-pic">Save changes</button>
+        </div>
+        </div>
     </div>
-    <div class="user-email">
-        <h4>Your E-mail:<span class="email"></span></h4>
-        <h4>Your username:<span class="email"></span></h4>
     </div>
-    <div class="password">
-        <a href="reset.php">Reset my password</a>
-    </div>
+    <h4>Your E-mail:<span class="email"></span></h4>
+    <h4>Your username:<span class="email"></span></h4>
+    <a href="reset.php">Reset my password</a>
 </div>
+<?php require "footer.php";?>

f572ed56ec121ba026059f27909bdf9485c608f3
ealpha072 <ealpha072@gmail.com>
2021-02-22 17:20:27 +0300

    Update functions for updating passwords

--- reset.php
+++ reset.php
@@ -8,7 +8,7 @@
   }
 
   if(isset($_POST['passreset'])){
-      //converting session variables to simpler vars
+    //converting session variables to simpler vars
     $useremail = $_SESSION['user_email'];
     $userpass = $_SESSION['password'];
 
@@ -16,17 +16,35 @@
     $oldpass = $_POST['oldpassword'];
     $newpass = $_POST['newpassword'];
     $confirmpass = $_POST['newpassword1'];
+    $usererrors =[];
     
     //prepairing sql
 
-    $sql = $conn->prepare("SELECT * FROM users where email='$useremail' AND password='$userpass'");
+    /*$sql = $conn->prepare("SELECT * FROM users where email='$useremail' AND password='$userpass'");
     $sql->execute();
     $num_rows = $sql->rowCount();
-    $result = $sql->fetchAll(PDO::FETCH_ASSOC);
-    
-    if($num_rows===1){
-        echo "Result found";
+    $result = $sql->fetchAll(PDO::FETCH_ASSOC);*/
+
+    if($newpass != $confirmpass){
+        array_push($usererrors, "Password mismatch");
+        echo "Your passwords dont match";
     }
+
+    if(count($usererrors)===0){
+        $oldpass=md5($oldpass);
+        $encrypt_newpass=md5($newpass);
+
+        if($oldpass != $userpass){
+            echo "Passwords mismatch";
+        }else{
+            $sql = $conn->prepare("UPDATE users SET password='$encrypt_newpass' WHERE email='$useremail' ");
+            $sql->execute();
+            $_SESSION['msg'] = "Successfully updated password";
+            header('location: profile.php');
+        }
+        
+    }
+    
   }
 ?>
 
@@ -40,15 +58,15 @@
                 <!-- Input fields -->
                 <div class="form-group">
                     <label for="oldpassword">Old password</label>
-                    <input type="text" class="form-control oldpassword" id="oldpassword" placeholder="Old password..." name="oldpassword">
+                    <input type="password" class="form-control oldpassword" id="oldpassword" placeholder="Old password..." name="oldpassword" required>
                 </div>
                 <div class="form-group">
                     <label for="password">New Password:</label>
-                    <input type="password" class="form-control new-password" id="new-password" placeholder="New Password..." name="newpassword">
+                    <input type="password" class="form-control new-password" id="new-password" placeholder="New Password..." name="newpassword" required>
                 </div>
                 <div class="form-group">
                     <label for="password">Confirm new Password</label>
-                    <input type="password" class="form-control newpassword1" id="newpassword1" placeholder="Password..." name="newpassword1">
+                    <input type="password" class="form-control newpassword1" id="newpassword1" placeholder="Password..." name="newpassword1" required>
                 </div>
 
                 <button type="submit" class="btn btn-primary btn-customized" name="passreset">Reset Password</button>

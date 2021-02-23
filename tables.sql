43bad5090382cfdee8bb86444500964ddbdae518
ealpha072 <ealpha072@gmail.com>
2021-02-23 20:12:04 +0300

    Code for inserting books

---
+++ tables.sql
@@ -0,0 +1,11 @@
+INSERT INTO `books`(`id`, `book_name`, `book_author`, `subject`) VALUES (1,'Management Accounting','R.J.Reddy','Accounting')
+
+INSERT INTO `books`(`id`, `book_name`, `book_author`, `subject`) VALUES 
+(2,'Financial Accounting','Ashok Banerjee','Accounting'),
+(3,'Fundamentals of Accounting','Donatila Agtarap-San Juan','Accounting'),
+(4,'Financial Accounting for BBA','S.N. Maheshwari ','Accounting'),
+(5,'Intermediate Accounting'.'Donald E. Kieso','Accounting'),
+(6,'BASIC FINANCIAL ACCOUNTING','Toye Adelaja','Accounting'),
+(7,'Accounting All-in-One For Dummies','Kenneth Boyd','Accounting'),
+(8,'Corporate Accounting, 6th Edition','Maheshwari S.N. & Maheshwari S.K.','Accounting'),
+(9,'Accounting For Management','Vijayakumar','Accounting')

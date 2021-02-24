4457f85c2bc0b3185c44588a96feba0d9fb78831
ealpha072 <ealpha072@gmail.com>
2021-02-24 14:13:43 +0300

    Input for file upload

--- registration.php
+++ registration.php
@@ -7,7 +7,7 @@
 <div class="row h-100 justify-content-center align-items-center">
   <div class="col-10 col-md-8 col-lg-6">
     <div class="reg-form">
-      <form action="registration.php" method="post">
+      <form action="registration.php" method="post" enctype="multipart/form-data">
         <div class="form-group input-group mb-3">
           <!--<label for="email">Email address</label>-->
           <div class="input-group-prepend">
@@ -35,6 +35,13 @@
             <span class="input-group-text" id="basic-addon1"><i class="fa fa-unlock" aria-hidden="true"></i></span>
           </div>
           <input type="password" class="form-control" id="" placeholder="Confirm Password" name="password_2">
+        </div>
+        <div class="form-group input-group mb-3">
+          <!--<label for="password">Confirm Password</label>-->
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+          </div>
+          <input type="file" class="form-control" id="" value="" name='userimage'>
         </div>
         <button type="submit" class="btn btn-primary" name="register"><i class="fa fa-paper-plane" aria-hidden="true"></i> Register</button>
       </form>

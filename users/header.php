17426a809bba6d692dbc4867230fd8756ee2307c
ealpha072 <ealpha072@gmail.com>
2021-03-08 11:43:57 +0300

    Removed commented code

--- users/header.php
+++ users/header.php
@@ -4,11 +4,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv = "x-ua-compatible" content="ie=edge"/>
   <title>Alpha Library</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-  crossorigin="anonymous">
-  <script src="https://use.fontawesome.com/c6dbced80b.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
- <div class="container-fluid">
+ <!--<div class="d-flex container-fluid">-->
+
+
+ 
+ 

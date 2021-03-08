54fec846c096fd7253a207e06bc5c7b563c10e55
ealpha072 <ealpha072@gmail.com>
2021-03-08 12:23:30 +0300

    Added icons and final code refinements

--- users/index.php
+++ users/index.php
@@ -30,19 +30,18 @@
 			<h5 class="username"><?php echo ucfirst($_SESSION["username"]);?></h5>
 		</div>
         <div class="list-group list-group-flush">
-            <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">My Profile</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">My books</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>            
+            <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fa fa-user" aria-hidden="true"></i> My Profile</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fa fa-book" aria-hidden="true"></i> My books</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fa fa-info-circle" aria-hidden="true"></i> Profile</a>            
         </div>
         </div>
         <!-- /#sidebar-wrapper -->
 
         <!-- Page Content -->
         <div id="page-content-wrapper">
-
             <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                <button class="btn btn-primary" id="menu-toggle">Hide/Show</button>
+                <button class="btn btn-primary" id="menu-toggle"> Hide/Show</button>
 
                 <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               		<span class="navbar-toggler-icon"></span>
@@ -69,10 +68,10 @@
                     			My Profile
                   			</a>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">View Profile</a>
-                                <a class="dropdown-item" href="#">Password</a>
+                                <a class="dropdown-item" href="#"><i class="fa fa-user" aria-hidden="true"></i> View Profile</a>
+                                <a class="dropdown-item" href="#"><i class="fa fa-unlock" aria-hidden="true"></i> Password</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Logout</a>
+                                <a class="dropdown-item" href="#"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a>
                             </div>
                         </li>
                     </ul>
@@ -80,8 +79,128 @@
             </nav>
 
             <div class="container-fluid">
-                <h1 class="mt-4">Simple Sidebar</h1>
-                
+                <!--main content goes here-->
+				<div class="row user-info-holder">
+					<!--holds the display divs for borrowed bks etc-->
+					<div class="col b-b">
+						<?php
+							$user_boorowed_books->execute();
+							$num_rows =$user_boorowed_books->rowCount();	
+						?>
+						<h1 class="user-books"><?php echo $num_rows;?></h1>
+						<h4>Borrowed books</h4>
+					</div>
+					<div class="col f-c">
+						<h1 class="fines">40</h1>
+						<h4>Fines collected</h4>
+					</div>
+					<div class="col o-t">
+						<h1 class="returned">3</h1>
+						<h4>Returned</h4>
+					</div>
+					<div class="col c-e">
+						<h1 class="credits">100</h1>
+						<h4>Credits Earned</h4>
+					</div>
+				</div>
+				<hr>
+				
+				<div class="result-holder">
+  					<!--search results go here-->
+					<?php
+						if (isset($_POST['borrowed_books'])){
+							$results = $user_boorowed_books->fetchAll(PDO::FETCH_ASSOC);?>
+
+								<table class="table table-striped table-dark">
+									<thead>
+										<tr>
+											<th scope="col">#</th>
+											<th scope="col">Title</th>
+											<th scope="col">Author</th>
+											<th scope="col">Subject</th>
+										</tr>
+									</thead>
+									<tbody>
+							<?php
+							foreach($results as $row){
+								$row_id =$row['book_id'];
+								$sql1 =$conn->prepare("SELECT * FROM books WHERE id=$row_id");
+								$sql1->execute();
+								$results1 =$sql1->fetchAll(PDO::FETCH_ASSOC);
+								
+								foreach ($results1 as $line) {?>
+									<tr>
+										<td><?php echo $line['id'];?></td>
+										<td><?php echo $line['book_name'];?></td>
+										<td><?php echo $line['book_author'];?></td>
+										<td><?php echo $line['subject'];?></td>
+									</tr>
+								<?php } }?>
+									</tbody>
+								</table>
+					<?php }
+
+					if(isset($_POST['search'])){
+						$searched_book=$_POST['book-search'];
+						$sql = $conn->prepare("SELECT * FROM books WHERE book_name='$searched_book'");
+						$sql->execute();
+						$result = $sql->fetchAll(PDO::FETCH_ASSOC);?>
+
+						<table class="table table-striped table-dark">
+							<thead>
+								<tr>
+									<th scope="col">#</th>
+									<th scope="col">Title</th>
+									<th scope="col">Author</th>
+									<th scope="col">Subject</th>
+									<th scope="col">Action</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach($result as $row){?>
+									<tr>
+										<td><?php echo $row['id'];?></td>
+											<td><?php echo $row['book_name'];?></td>
+											<td><?php echo $row['book_author'];?></td>
+											<td><?php echo $row['subject'];?></td>
+											<td><a href="borrow.php?id=<?php echo $row[" id "];?>">Borrow</a></td>
+									</tr>
+								<?php }?>
+							</tbody>
+						</table>
+					<?php } 
+
+					//refactor from this point
+					if(isset($_POST['allbooks'])){
+						$sql = $conn->prepare("SELECT * FROM books");
+						$sql->execute();
+						$result = $sql->fetchAll(PDO::FETCH_ASSOC);?>
+
+						<table class="table table-striped table-dark">
+							<thead>
+								<tr>
+									<th scope="col">#</th>
+									<th scope="col">Title</th>
+									<th scope="col">Author</th>
+									<th scope="col">Subject</th>
+									<th scope="col">Action</th>
+								</tr>
+							</thead>
+							<tbody>
+							<?php foreach($result as $row){?>
+								<tr>
+									<td><?php echo $row['id'];?></td>
+									<td><?php echo $row['book_name'];?></td>
+									<td><?php echo $row['book_author'];?></td>
+									<td><?php echo $row['subject'];?></td>
+									<td><a href="borrow.php?id=<?php echo $row[" id "];?>"><button class="btn btn-success">Borrow</button></a></td>
+								</tr>
+							<?php }?>
+							</tbody>
+						</table>
+
+					<?php }?>
+				</div>               
             </div>
         </div>
         <!-- /#page-content-wrapper -->

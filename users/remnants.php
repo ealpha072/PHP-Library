899bbabd1805bb609665c8c462c312ae52f109ea
ealpha072 <ealpha072@gmail.com>
2021-03-09 21:29:24 +0300

    Moved contents of profile page here

--- users/remnants.php
+++ users/remnants.php
@@ -266,4 +266,38 @@
 								<?php } }?>
 									</tbody>
 								</table>
-					<?php }
+					<?php }?>
+
+//for prepend and append
+<div>
+        <h4>Your E-mail:<span class="email"> <?php echo $_SESSION["user_email"];?></span></h4>
+        <h4>Your username:<span class="email"> <?php echo ucfirst($_SESSION["username"]);?></span></h4>
+        <a href="reset.php">Reset my password</a><br>
+        <a href="index.php">Home</a>
+    </div>
+
+//user profile div 
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Change Profile Picture</button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">New Profile</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="" method="POST">
+                        <input type="file">
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" name="profile-pic">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>

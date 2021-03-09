e129a1f342bea4fbe17f24d6bc6b0965674207a7
ealpha072 <ealpha072@gmail.com>
2021-03-09 18:40:31 +0300

    Minor fixes

--- users/index.php
+++ users/index.php
@@ -30,6 +30,7 @@
 		</div>
         <div class="list-group list-group-flush">
             <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard</a>
+			<a href="userbooks.php" class="list-group-item list-group-item-action bg-light"><i class="fa fa-user" aria-hidden="true"></i> My Books</a>
             <a href="profile.php" class="list-group-item list-group-item-action bg-light"><i class="fa fa-user" aria-hidden="true"></i> My Profile</a>
             <a href="" class="list-group-item list-group-item-action bg-light"><i class="fa fa-info-circle" aria-hidden="true"></i> About</a>            
         </div>
@@ -135,36 +136,5 @@
 							</tbody>
 						</table>
 					<?php } ?>
-					<?php	$results = $user_boorowed_books->fetchAll(PDO::FETCH_ASSOC);?>
-								<h3>Your books</h3>	
-								<table class="table table-striped table-dark">
-									<thead>
-										<tr>
-											<th scope="col">#</th>
-											<th scope="col">Title</th>
-											<th scope="col">Author</th>
-											<th scope="col">Subject</th>
-										</tr>
-									</thead>
-									<tbody>
-							<?php
-							foreach($results as $row){
-								$row_id =$row['book_id'];
-								$sql1 =$conn->prepare("SELECT * FROM books WHERE id=$row_id");
-								$sql1->execute();
-								$results1 =$sql1->fetchAll(PDO::FETCH_ASSOC);
-								
-								foreach ($results1 as $line) {?>
-									<tr>
-										<td><?php echo $line['id'];?></td>
-										<td><?php echo $line['book_name'];?></td>
-										<td><?php echo $line['book_author'];?></td>
-										<td><?php echo $line['subject'];?></td>
-									</tr>
-								<?php } }?>
-									</tbody>
-								</table>
-
-				
-
-<?php require "footer.php"; ?>
+					
+<?php  require "footer.php"; ?>

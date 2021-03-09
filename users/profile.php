c1d34b137b926a9aac34c2f15587434b1596312c
ealpha072 <ealpha072@gmail.com>
2021-03-09 21:39:32 +0300

    Added a form and a card for the user

--- users/profile.php
+++ users/profile.php
@@ -8,23 +8,36 @@
   }
 ?>
 <div class="user-profile"> 
+    <h2>My Profile</h2>
     <div class="row">
         <div class="col userimage">
-
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="../images/download.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
         </div>
         <div class="col updateprofile">
-
+            <form action="" method="POST">
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1">@</span>
+                    </div>
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+            </form>
         </div>
     </div>
     
-    <form action="" method="POST">
+    <!--<form action="" method="POST">
         <div class="input-group mb-3">
             <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">@</span>
             </div>
             <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
         </div>
-    </form>
+    </form>-->
     
 </div>
 <?php require "footer.php";?>

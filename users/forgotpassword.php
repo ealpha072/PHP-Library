6ba66b02fbe2452f89524594821609c1d6af56f8
ealpha072 <ealpha072@gmail.com>
2021-03-10 12:29:25 +0300

    Changes append to prepend

--- users/forgotpassword.php
+++ users/forgotpassword.php
@@ -1,23 +1,27 @@
 <?php require "header.php";?>
 
-<div class="card">
-    <div class="card-body login-card-body">
-        <p class="login-box-msg">Fill in your email to receive a reset link to your email.</p>
-        <form method="POST" action="">
-            <div class="input-group mb-3">
-                <input id="email" type="email" class="form-control " name="email" value="" required="" autocomplete="email" autofocus="" placeholder="Enter Email To Receive Reset Link">
-                <div class="input-group-append">
-                    <div class="input-group-text">
-                        <span class="fas fa-envelope-o"></span>
+<div class="row h-100 justify-content-center align-items-center">
+    <div class="col-10 col-md-8 col-lg-6">
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="text-center">Fill in your email to receive a reset link to your email.</p>
+                <form method="POST" action="">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <span class="fa fa-envelope-o"></span>
+                            </div>
+                        </div>
+                        <input type="email" class="form-control " name="email" value="" required="" autocomplete="email" autofocus="" placeholder="Enter Email To Receive Reset Link">
                     </div>
-                </div>
-            </div>
 
-            <div class="row">
-                <div class="col-12">
-                    <button type="submit" class="btn btn-dark btn-block"> Send Password Reset Link</button>
-                </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-dark btn-block"> Send Password Reset Link</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
 </div>

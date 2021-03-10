80bab4bc8684d1d09f6ef45a58c16408a968807e
ealpha072 <ealpha072@gmail.com>
2021-03-10 13:42:13 +0300

    Moved js code to this file

--- js-files/script.js
+++ js-files/script.js
@@ -1,2 +1,16 @@
-const dashboard = document.querySelector("#dashboard");
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
 
+$(".changepassword").click(function(e) {
+    e.preventDefault();
+    $(".updateprofile").hide(1000);
+    $(".passwordchange").show(1000);
+})
+
+$(".details").click(function(e) {
+    e.preventDefault();
+    $(".passwordchange").hide(1000);
+    $(".updateprofile").show(1000);
+})

8c0324e46c9a8aca56d44cc72d9f1ac9fbaf0cf1
ealpha072 <ealpha072@gmail.com>
2021-03-10 10:24:33 +0300

    Removed the wire: attribute from form inputs

--- users/profile.php
+++ users/profile.php
@@ -29,53 +29,52 @@
             <div class="card">
                 <div class="card-header p-2">
                     <ul class="nav nav-pills">
-                        <li class="nav-item"><a class="nav-link active" wire:click="$set('tab', 1)" href="#" data-toggle="tab">My Details</a></li>
-                        <li class="nav-item"><a class="nav-link " wire:click="$set('tab', 2)" href="#" data-toggle="tab">Change Password</a></li>
+                        <li class="nav-item"><a class="nav-link active"  href="#" data-toggle="tab">My Details</a></li>
                     </ul>
                 </div><!-- /.card-header -->
                 <div class="card-body">
                     <div class="tab-content">
                         <div class="tab-pane active">
-                            <form class="form-horizontal" wire:submit.prevent="saveProfile">
-                                <input type="hidden" name="_token" value="9QM5rt7kDVjhGGKhx90B2qfpCbfJo5pP2dAEZhoE">
+                            <form class="form-horizontal">
+                                <input type="hidden" name="_token">
                                 <div class="mb-2">
                                     <div class="input-group">
                                         <div class="input-group-prepend prf"><span class="input-group-text ">Upl. Image</span> </div>
-                                        <input type="file" class="form-control  text-sm" accept=".jpg,.jpeg,.png" wire:model="photo">
+                                        <input type="file" class="form-control  text-sm" accept=".jpg,.jpeg,.png">
                                     </div>
                                 </div>
                                 <div class="mb-2">
                                     <div class="input-group"><div class="input-group-prepend prf"><span class="input-group-text ">Upl. Proof<i class="fas fa-info-circle ml-2" data-toggle="tooltip" data-placement="top" title="" data-original-title="Either images/pdf are allowed."></i></span> </div>
-                                        <input type="file" class="form-control  text-sm" wire:model="proof" accept=".jpg,.jpeg,.png,.pdf">                                                   
+                                        <input type="file" class="form-control  text-sm">                                                   
                                     </div>
                                 </div>
                                 <div class="mb-2">
                                     <div class="input-group"><div class="input-group-prepend prf"><span class="input-group-text ">Full Name</span> </div>
-                                        <input type="text" class="form-control" wire:model.defer="name">
+                                        <input type="text" class="form-control" >
                                     </div>
                                 </div>
                                 <div class="mb-2">
                                     <div class="input-group"><div class="input-group-prepend prf"><span class="input-group-text ">Email</span> </div>
-                                        <input type="email" wire:model="email" class="form-control" readonly="">
+                                        <input type="email" class="form-control" readonly="">
                                     </div>
                                 </div>
                                 <div class="mb-2">
                                     <div class="input-group"><div class="input-group-prepend prf"><span class="input-group-text ">Phone</span> </div>
-                                        <input type="text" class="form-control" wire:model.defer="phone">
+                                        <input type="text" class="form-control">
                                     </div>
                                 </div>                           
                                 <div class="mb-2">
                                     <div class="input-group"><div class="input-group-prepend prf"><span class="input-group-text ">Address</span> </div>
-                                        <textarea class="form-control" wire:model.defer="address"></textarea>
+                                        <textarea class="form-control" ></textarea>
                                     </div>
                                 </div>
                                 <div class="mb-2">
                                     <div class="input-group">
                                         <div class="input-group-prepend">
                                             <div class="form-check">
-                                                <input wire:model.defer="check" class="form-check-input" type="checkbox" value="" id="tacCheckChecked" checked="">
-                                                <label class="form-check-label" for="tacCheckChecked">
-                                                    I Agree <a target="_blank" href="/terms-and-conditions ">Terms and conditions.</a>
+                                                <input class="form-check-input" type="checkbox" value="" checked="">
+                                                <label class="form-check-label" for="">
+                                                    I Agree <a target="_blank" href="">Terms and conditions.</a>
                                                 </label>
                                             </div>
                                         </div>
@@ -83,21 +82,17 @@
                                 </div>
                                 <div class="mb-2">
                                     <div class="input-group">
-                                        <button type="submit" class="btn btn-sm btn-dark"><i class="fas fa-save mr-1"></i>Save Profile
+                                        <button type="submit" class="btn btn-sm btn-dark"><i class="fa fa-save mr-1"></i>Save Profile
                                         </button>
                                     </div>
                                 </div>
 
                             </form>
-                        </div>
-                                        <!-- /.tab-pane -->                                        
-                    </div>
-                                    <!-- /.tab-content -->
+                        </div><!-- /.tab-pane -->                                        
+                    </div><!-- /.tab-content -->
                 </div><!-- /.card-body -->
-            </div>
-                            <!-- /.nav-tabs-custom -->
-        </div>
-                        <!-- /.col -->
+            </div><!-- /.nav-tabs-custom -->
+        </div><!-- /.col -->
     </div>
     
 </div>

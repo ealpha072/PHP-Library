d3fb4bc175aa31d108cb7b5c8c966353865f4ad0
ealpha072 <ealpha072@gmail.com>
2021-03-10 13:17:35 +0300

    Values added to select options

--- users/registration.php
+++ users/registration.php
@@ -7,20 +7,24 @@
 		<div class="reg-form">
 			<form action="registration.php" method="post" enctype="multipart/form-data">
 				<h4 class="text-center">Register to be a member</h4>
+
+				<!--email field-->
 				<div class="form-group input-group mb-3">
 					<div class="input-group-prepend">
 						<span class="input-group-text" id="basic-addon1"><i class="fa fa-envelope" aria-hidden="true"></i></span>
 					</div>
 					<input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
 				</div>
-			
+
+				<!--fullname field-->
 				<div class="form-group input-group mb-3">				
 					<div class="input-group-prepend">
 						<span class="input-group-text" id="basic-addon1"><i class="fa fa-user" aria-hidden="true"></i></span>
 					</div>
-					<input type="text" class="form-control" id="fullname" placeholder="Full name" name="fullname" required>
+					<input type="text" class="form-control" id="fullname" placeholder="Full name" name="username" required>
 				</div>
-
+				
+				<!--phonenumber field-->
 				<div class="form-group input-group mb-3">				
 					<div class="input-group-prepend">
 						<span class="input-group-text" id="basic-addon1"><i class="fa fa-phone" aria-hidden="true"></i></span>
@@ -28,21 +32,23 @@
 					<input type="number" class="form-control" placeholder="Phone Number" name="phonenumber" required maxlength="10">
 				</div>
 
+				<!--course/year field-->
 				<div class="form-group input-group mb-3">
-					<select name="" id="" class="form-control" required>
+					<select id="" class="form-control" required name="course">
 						<option value="">--Select--</option>
-						<option value="">MSC</option>
-						<option value="">MBA</option>
+						<option value="MSC">MSC</option>
+						<option value="MBA">MBA</option>
 					</select>
-					<select name="" id="" class="form-control" required>
+					<select class="form-control" required name="studyyear">
 						<option value="">--Select--</option>
-						<option value="">1st Year</option>
-						<option value="">2nd Year</option>
-						<option value="">3rd Year</option>
-						<option value="">4th Year</option>
+						<option value="1">1st Year</option>
+						<option value="2">2nd Year</option>
+						<option value="3">3rd Year</option>
+						<option value="4">4th Year</option>
 					</select>
 				</div>
 
+				<!--password_1 field-->
 				<div class="form-group input-group mb-3">
 					<div class="input-group-prepend">
 						<span class="input-group-text" id="basic-addon1"><i class="fa fa-unlock" aria-hidden="true"></i></span>
@@ -50,6 +56,7 @@
 					<input type="password" class="form-control" id="password" placeholder="Password" name="password_1" required>
 				</div>
 
+				<!--password_2 field-->
 				<div class="form-group input-group mb-3">
 					<!--<label for="password">Confirm Password</label>-->
 					<div class="input-group-prepend">
@@ -58,16 +65,19 @@
 					<input type="password" class="form-control" id="" placeholder="Confirm Password" name="password_2" required>
 				</div>
 
+				<!--userimage field-->
 				<div class="form-group input-group mb-3 b-lr-1">					
                         <div class="input-group-prepend prf"><span class="input-group-text ">Photo</span> </div>
                         <input type="file" class="form-control  text-sm" accept=".jpg,.jpeg,.png" name="userimage">
 				</div>
 
+				<!--userimageproof field-->
 				<div class="form-group input-group mb-3 b-lr-1">					
                         <div class="input-group-prepend prf"><span class="input-group-text ">Upl Proof</span> </div>
                         <input type="file" class="form-control  text-sm" accept=".jpg,.jpeg,.png" name="userimageproof">
 				</div>
 
+				<!--submitbtn/terms field-->
 				<div class="row">
 					<div class="col-8">
 						<div class="icheck-primary">

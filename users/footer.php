92d243e5d3be3ba62dd60efc1d6e150d77082ee9
ealpha072 <ealpha072@gmail.com>
2021-03-10 10:53:01 +0300

    Jquery code for hidding and showing details and passwords div

--- users/footer.php
+++ users/footer.php
@@ -13,6 +13,19 @@
             e.preventDefault();
             $("#wrapper").toggleClass("toggled");
         });
+
+        $(".changepassword").click(function(e){
+            e.preventDefault();
+            $(".updateprofile").hide(1000);
+            $(".passwordchange").show(1000);
+        })
+
+        $(".details").click(function(e){
+            e.preventDefault;
+            $(".passwordchange").hide(1000);
+            $(".updateprofile").show(1000);
+        })
+
     </script>
 </body>
 </html>

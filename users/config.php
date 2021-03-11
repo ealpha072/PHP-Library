23e73d7cf53575b282536f4303d9797e768bc973
ealpha072 <ealpha072@gmail.com>
2021-03-11 11:43:41 +0300

    Code for updating phone number and address of user

--- users/config.php
+++ users/config.php
@@ -54,23 +54,96 @@
   }
 
   //register code
-  if(isset($_POST['register'])){
-    try{ 
-		//setting variables   
-    	global $conn;
-      	$error =[];      	
-      	$email = $_POST['email'];
-		$username = $_POST['username'];
-		$phone =$_POST['phonenumber'];
-		$course =$_POST['course'];
-		$study_year=$_POST['studyyear'];
-      	$password_1 = $_POST['password_1'];
-      	$password_2 = $_POST['password_2'];
+	if(isset($_POST['register'])){
+		try{ 
+			//setting variables   
+			global $conn;
+			$error =[];      	
+			$email = $_POST['email'];
+			$username = $_POST['username'];
+			$phone =$_POST['phonenumber'];
+			$course =$_POST['course'];
+			$study_year=$_POST['studyyear'];
+			$password_1 = $_POST['password_1'];
+			$password_2 = $_POST['password_2'];
+			$address =$_POST['address'];
 
-		$filename =$_FILES['userimage']['name'];
-		$file =$_FILES['userimage']['tmp_name'];
-		$destination = "uploads/".$filename;
-		$extension =pathinfo($filename, PATHINFO_EXTENSION);
+			$filename =$_FILES['userimage']['name'];
+			$file =$_FILES['userimage']['tmp_name'];
+			$destination = "uploads/".$filename;
+			$extension =pathinfo($filename, PATHINFO_EXTENSION);
+
+			if(!in_array($extension,['png','jpg','jpeg'])){
+					echo "You file extension type is not allowed";
+				}else{
+					if(move_uploaded_file($file,$destination)){
+						echo "Success";
+					}else{
+						echo  "Failed";
+				}
+			}
+			
+			//check for password match
+			if(!($password_1===$password_2)){
+				echo "Password mismatch, please provide matching passwords!";
+				array_push($error," Error");
+			}
+
+			//check if email exists in database
+			$sql1 = $conn->prepare("SELECT * FROM users WHERE email='$email' OR user_name ='$username'");
+			$sql1->execute();
+			$results = $sql1->rowCount();
+
+			if($results>1){
+				array_push($error,"User exists");
+				echo "Username or email already taken!!";
+			}
+
+			//if everything is okay
+			if(count($error)===0){
+
+				//password processing and database entry
+				$password_1 = md5($password_1);
+				$sql = "INSERT INTO users (email,user_name,password,user_image,phone,course,study_year,address) 
+						VALUES ('$email','$username','$password_1',
+								'$filename','$phone','$course','$study_year','$address'
+						)";
+				$newResults=$conn->query($sql);
+				
+				//get last inserted id
+				$last_id = $conn->lastInsertId();
+
+				//session variables
+				$_SESSION['id'] =$last_id;
+				$_SESSION['loggedin']=true;
+				$_SESSION['username'] = $username;
+				$_SESSION['success'] = 'You are now logged in';
+				$_SESSION['user_email']=$email;
+				$_SESSION['password']=$password_1;
+				$_SESSION['image']= $filename;
+				$_SESSION['course'] =$course;
+				$_SESSION['yr']=$study_year;
+				
+				//redirect users
+				header('location:index.php');
+			}
+
+		}catch(Exception $e){
+		die ("Error: Could not execute .$sql1 ".$e->getMessage());
+		}
+	}
+
+	if(isset($_POST['save-profile'])){
+		$id =$_SESSION['id'];
+		//
+		$phone =$_POST['number'];
+		$address =$_POST['address'];
+		/*
+		//image
+		$filename =$_FILES['user_image']['name'];
+		$file =$_FILES['user_image']['tmp_name'];
+		$destination="upload/".$filename;
+		$extension=pathinfo($filename,PATHINFO_EXTENSION);
 
 		if(!in_array($extension,['png','jpg','jpeg'])){
 			echo "You file extension type is not allowed";
@@ -78,58 +151,18 @@
 			if(move_uploaded_file($file,$destination)){
 				echo "Success";
 			}else{
-				echo  "Failed";
+				echo 'Failed';
 			}
-		}
+		}*/
+
+		$sql =$conn->prepare("UPDATE users SET phone='$phone', address='$address' WHERE id=$id");
+		$sql->execute();
+
+		header("Location: index.php");
+
 		
-      	//check for password match
-    	if(!($password_1===$password_2)){
-			echo "Password mismatch, please provide matching passwords!";
-			array_push($error," Error");
-    	}
 
-      	//check if email exists in database
-      	$sql1 = $conn->prepare("SELECT * FROM users WHERE email='$email' OR user_name ='$username'");
-      	$sql1->execute();
-      	$results = $sql1->rowCount();
 
-      	if($results>1){
-        	array_push($error,"User exists");
-        	echo "Username or email already taken!!";
-      	}
-
-		//if everything is okay
-      	if(count($error)===0){
-
-			//password processing and database entry
-			$password_1 = md5($password_1);
-			$sql = "INSERT INTO users (email,user_name,password,user_image,phone,course,proof,study_year) 
-					VALUES ('$email','$username','$password_1',
-							'$filename','$phone','$course',0,'$study_year'
-					)";
-			$newResults=$conn->query($sql);
-			
-			//get last inserted id
-			$last_id = $conn->lastInsertId();
-
-			//session variables
-			$_SESSION['id'] =$last_id;
-			$_SESSION['loggedin']=true;
-			$_SESSION['username'] = $username;
-			$_SESSION['success'] = 'You are now logged in';
-			$_SESSION['user_email']=$email;
-			$_SESSION['password']=$password_1;
-			$_SESSION['image']= $filename;
-			$_SESSION['course'] =$course;
-			$_SESSION['yr']=$study_year;
-			
-			//redirect users
-			header('location:index.php');
-      	}
-
-    }catch(Exception $e){
-      die ("Error: Could not execute .$sql1 ".$e->getMessage());
-    }
-}
+	}
 
 ?>
